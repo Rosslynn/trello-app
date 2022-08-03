ee8ac442089fce5b16710f0bb700e3de332ddf01
Lynross <mauriciobarva@gmail.com>
2022-08-02 23:44:34 -0500

    db.json added

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import Vue2 from '@vitejs/plugin-vue2';
+
+export default defineConfig({
+  plugins: [
+    Vue2(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: [{ find: /^vue$/, replacement: 'vue/dist/vue.runtime.common.js' }],
+  },
+});
