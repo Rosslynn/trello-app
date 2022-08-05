ecd529e8d3d0bdeaae66c503a7dbce478603223d
Lynross <mauriciobarva@gmail.com>
2022-08-04 21:02:24 -0500

    feat: boards container component updated

--- src/components/BoardsContainerComponent.vue
+++ src/components/BoardsContainerComponent.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    hello world
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>{{ title }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BoardsContainerComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    boards: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
