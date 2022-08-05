c9c0a1a22f038ae52e1aa41b0958a3bf410bc147
Lynross <mauriciobarva@gmail.com>
2022-08-04 22:46:40 -0500

    feat: docs added

--- src/services/boardsService.js
+++ src/services/boardsService.js
@@ -2,11 +2,20 @@ import { makeGetRequest } from '../clients/clientWrapper';
 
 const baseURL = 'http://localhost:3000';
 
+/**
+ * Función para obtener los tableros
+ * @returns Promesa con la respuesta
+ */
 export function getBoards() {
   const url = `${baseURL}/boards`;
   return makeGetRequest(url);
 }
 
+/**
+ * Función para crear un tablero
+ * @param {Object} body - Información del tablero
+ * @returns Promesa con la respuesta
+ */
 export function addBoard(body) {
   const url = `${baseURL}/boards`;
   return makeGetRequest(url, body);
