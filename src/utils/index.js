8a21cf8efa5b94182210cbd1e10debcd246ddb76
Lynross <mauriciobarva@gmail.com>
2022-08-09 23:22:18 -0500

    feat: clg removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,6 @@ import store from '../store/index';
  * @param {*} valueToShow - Valor a retornar cada vez que se use la propiedad - (Debe ser un getter)
  */
 export function defineReactiveProperty(objectToAddProperty, propertyName, valueToShow) {
-  console.log(valueToShow);
   Object.defineProperty(objectToAddProperty, propertyName, {
     enumerable: true,
     get: () => store.getters[valueToShow],
